c4ba9d0d9ecf2107a70dff4693e12504f7cb128e
mdjikrulislamjeto <md.jikrulislamjeto@gmail.com>
2026-09-13 18:50:15 +0600

    footer section added

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@
 import Navbar from "./components/Navbar";
 import Hero from "./components/Hero";
 import TechExplorer from "./components/TechExplorer";
+import Footer from "./components/Footer";
 
 function App() {
   return (
@@ -12,7 +13,10 @@ function App() {
       <main>
         <Hero />
         <TechExplorer />
+
       </main>
+
+      <Footer />
 
     </>
   );
